5d578d5e9597bd31a1f97741d4554d17191c78dc
Bruno Ribas Torres <brunotorreseng@gmail.com>
2024-08-08 15:30:15 -0300

    exercise review

--- CodeCademy/Learnjava/HelloWorld/LanguageFacts.java
+++ CodeCademy/Learnjava/HelloWorld/LanguageFacts.java
@@ -4,7 +4,9 @@ public class LanguageFacts {
     public static void main(String[] args) {
     
     System.out.println("Java is a class-based language.");
+    
     System.out.println("Java classes have a 'main' method.");
+    
     System.out.println("Java statements end with a semicolon.");
 
 
